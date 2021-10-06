88c0af1caa261e7a764ca20801ccc9316db1ef9f
PeteLaric <52544596+PeteLaric@users.noreply.github.com>
2021-10-05 19:00:16 -0700

    Update install_xmrig.sh
    Corrected path error on last line (the line that actually runs xmrig for the first time).

--- install_xmrig.sh
+++ install_xmrig.sh
@@ -13,4 +13,4 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make
-./xmrig/build/xmrig -o pool.minexmr.com:4444 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
+~/xmrig/build/xmrig -o pool.minexmr.com:4444 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
