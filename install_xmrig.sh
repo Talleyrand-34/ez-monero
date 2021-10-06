8914c8e6d8beb7213845c67a46f196f837df2333
PeteLaric <52544596+PeteLaric@users.noreply.github.com>
2021-10-05 18:39:08 -0700

    Added the installation script.

---
+++ install_xmrig.sh
@@ -0,0 +1,16 @@
+# install_xmrig.sh
+# A shell script to install xmrig (Monero cryptocurrency mining software) on any Linux machine.
+# (cc) BY Pete Laric / http://www.PeteLaric.com
+# Stolen from https://blockforums.org/topic/1257-how-to-mine-monero-and-compile-xmrig-on-a-raspberry-pi-4-cpu-mining-in-2021/
+#
+# To run:
+#
+# sudo sh install_xmrig.sh
+#
+cd ~
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make
+./xmrig/build/xmrig -o pool.minexmr.com:4444 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
