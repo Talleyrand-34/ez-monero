58c71c9f526bab27607aed99e7708cd10ed214d3
PeteLaric <52544596+PeteLaric@users.noreply.github.com>
2021-10-05 19:07:29 -0700

    Added script to mine Monero on any Linux machine!
    First run:
    
    sudo sh install_xmrig.sh
    
    Then run:
    
    sh mine.sh

---
+++ mine.sh
@@ -0,0 +1,8 @@
+# mine.sh
+# A simple script to start mining Monero!
+#
+# To start mining:
+#
+# sh mine.sh
+#
+~/xmrig/build/xmrig -o pool.minexmr.com:4444 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
