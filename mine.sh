216f48c961559775cdea95a66e4a2d925f3b2cc3
PeteLaric <52544596+PeteLaric@users.noreply.github.com>
2022-12-19 04:38:02 -0700

    Update mine.sh
    Replaced now-defunct MineXMR pool with SupportXMR.

--- mine.sh
+++ mine.sh
@@ -5,4 +5,4 @@
 #
 # sh mine.sh
 #
-~/xmrig/build/xmrig -o pool.minexmr.com:4444 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
+~/xmrig/build/xmrig -o pool.supportxmr.com:3333 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
